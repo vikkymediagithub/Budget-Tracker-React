a4b89be013882c8bc25ff8a76d6ba80d519b9bc2
Hi, I'm Victor Joseph <okevictor14@gmail.com>
2024-12-12 09:08:58 +0300

    Transaction page

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,12 @@
+
+
+const Transaction = ({ transaction}) => {
+  return (
+    <li className="minus"> 
+       {transaction.text}
+     <span>-$400</span><button className="delete-btn">x</button>
+    </li>
+  )
+}
+
+export default Transaction
